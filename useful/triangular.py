ae4d53c8591aad77abba76da4da4ccec9fcf08fe
limbaksa <72548759+limbaksa@users.noreply.github.com>
2021-09-11 01:02:05 +0900

    Update triangular.py

--- useful/triangular.py
+++ useful/triangular.py
@@ -7,6 +7,8 @@ def cos(x):
 def tan(x):
     if (x-(math.radians(90)))%(math.radians(180)):
         return math.tan(x)
+    else:
+        return math.inf
 #no usage of asin/acos -> only needs atan
 def atan(x):
     return math.atan(x)
