c0fdf947e33a8ded97e5401963c599f68abd114f
limbaksa <72548759+limbaksa@users.noreply.github.com>
2021-09-11 01:18:46 +0900

    spherical coordinate->Cartesian coordinate
    who made the equatorial coordinate

--- observation.py
+++ observation.py
@@ -1,1 +1,8 @@
-def observation(d1,alpha1,beta1,d2,alpha2,beta2):
+from math import *
+#d=(AU),alpha=x->y,beta=xy->z
+def xobservation(d,alpha,beta):
+    return 1+(d*cos(beta)*cos(alpha))
+def yobservation(d,alpha,beta):
+    return d*cos(beta)*sin(alpha)
+def zobservation(d,alpha,beta):
+    return d*sin(beta)
