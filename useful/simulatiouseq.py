265c0e201c120fe1fa74dd7efed5a7afaa3c87bf
mungmungbaksa <blackkarn9023@naver.com>
2021-09-11 00:54:19 +0900

    simul edit

--- useful/simulatiouseq.py
+++ useful/simulatiouseq.py
@@ -1,5 +1,5 @@
 #ax+by+c=0,dx+ey+f=0
-def simultanious(a,b,c,d,e,f):
+def simultaniousx(a,b,c,d,e,f):
     x=((b*f)-(c*e))/((a*e)-(b*d))
+def simultaniousy(a,b,c,d,e,f):
     y=((c*d)-(a*f))/((a*e)-(b*d))
-    return [x,y]
