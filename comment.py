f1479e912a766c1d6afe9d2f2bc80fa66db64192
mungmungbaksa <blackkarn9023@naver.com>
2021-09-11 00:17:40 +0900

    defmn

--- comment.py
+++ comment.py
@@ -1,9 +1,17 @@
-#%%
 import math
-import matplotlib.pyplot as plt
+import matplotlib
 
-plt.plot([1,2,3,4])
-plt.show
+def sqrt(x):
+    return math.sqrt(x)
+def sin(x):
+    return math.sin(x)
+def cos(x):
+    return math.cos(x)
+def tan(x):
+    return math.tan(x)
 
+def defm(x,y,z,st,ct,so,co):
+    return pow(ct*(co*x-so*z),2)-2*ct*st(co*x-so*z)*y+pow(st*y,2)
+def defn(x,y,z,st,ct,so,co):
+    return pow(st*(co*x-so*z),2)-2*ct*st(co*x-so*z)*y+pow(ct*y,2)
 
-# %%
